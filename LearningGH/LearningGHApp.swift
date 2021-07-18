959ea84dc0b94c5819955c4ef638b6c86cc8cdd2
Will Archer <wdarcherjr@gmail.com>
2021-07-18 18:56:57 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Will Archer on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
